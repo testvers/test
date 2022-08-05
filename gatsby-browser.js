9afe2680889f5de0f355266ddf828e363809eafe
testvers <87796576+testvers@users.noreply.github.com>
2022-08-05 20:02:20 +0500

    push

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,11 @@ import store from './src/Global/store';
 import { Theme } from "./src/Global/theme";
 import './src/Global/Global.css';
 import { ApolloProvider } from '@apollo/client';
-import { client } from "./src/Apollo/client";
+//import { client } from "./src/Apollo/client";
 import {Providers} from './netlifyIdentityContext';
 
 export const wrapRootElement = ({ element }) => {
   
-  return <Providers><ApolloProvider client={client}><Provider store={store}><Theme>{element}</Theme></Provider></ApolloProvider></Providers>;
+  //return <Providers><ApolloProvider client={client}><Provider store={store}><Theme>{element}</Theme></Provider></ApolloProvider></Providers>;
+  return <Providers><Provider store={store}><Theme>{element}</Theme></Provider></Providers>;
 };
